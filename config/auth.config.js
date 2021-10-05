259bfb5062da6cf16d7a49411f21c3d0f4c1460a
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2021-10-05 12:26:24 -0400

    backend w/ users working heroku

---
+++ config/auth.config.js
@@ -0,0 +1,3 @@
+module.exports = {
+    secret: "james-secret-key"
+  };
---
+++ models/user.model.js
@@ -0,0 +1,18 @@
+module.exports = (sequelize, Sequelize) => {
+  const User = sequelize.define("users", {
+    username: {
+      type: Sequelize.STRING
+    },
+    email: {
+      type: Sequelize.STRING
+    },
+    password: {
+      type: Sequelize.STRING
+    },
+    contractor: {
+      type: Sequelize.BOOLEAN
+    }
+  });
+
+  return User;
+};
