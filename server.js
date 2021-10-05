8923d5fc27deb9986e393b40b0b26d19d6258876
jameslenahan <57712403+jameslenahan@users.noreply.github.com>
2021-10-05 13:09:56 -0400

    updated cors

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "http://localhost:3000"
+  origin: "https://bmp-frontend.herokuapp.com/"
 };
 
 app.use(cors(corsOptions));
